2eee4fa146697164c5d7bd2b142eab2c67990b8a
fblaze62 <feablaze@outlook.com>
2026-08-27 11:22:51 +0800

    style: 更改了页脚，删除了默认配置

--- docs/.vitepress/teek-config.ts
+++ docs/.vitepress/teek-config.ts
@@ -13,19 +13,19 @@ export const teekConfig = defineTeekConfig({
   sidebarTrigger: true,
   author: { name: "FeatherBlaze", link: "https://github.com/fblaze62" },
   blogger: {
-    name: "飞焰矩阵 文档",
-    slogan: "",
-    avatar: "https://gcore.jsdelivr.net/gh/fblaze62/fblaze-docs/docs/public/fm-logo-mini.png",
-    shape: "square",
-    circleBgImg: "/blog/bg4.webp",
-    color: "#ffffff",
-    circleSize: 120,
-    status: {
-      icon: "😪",
-      size: 28,
-      title: "困",
-    },
-  },
+  //   name: "飞焰矩阵 文档",
+  //   slogan: "",
+  //   avatar: "https://gcore.jsdelivr.net/gh/fblaze62/fblaze-docs/docs/public/fm-logo-mini.png",
+  //   shape: "square",
+  //   circleBgImg: "/blog/bg4.webp",
+  //   color: "#ffffff",
+  //   circleSize: 120,
+  //   status: {
+  //     icon: "😪",
+  //     size: 28,
+  //     title: "困",
+  //   },
+  // },
   articleAnalyze: {
     dateFormat: "yyyy-MM-dd hh:mm:ss",
   },
@@ -74,110 +74,110 @@ export const teekConfig = defineTeekConfig({
       createYear: 2026,
       suffix: "FeatherBlaze",
     },
-    customHtml: `<span id="runtime"></span>`, // 需要搭配 .vitepress/theme/helper/useRuntime.ts 使用
-    topMessage: [
-      `<span><img alt="VitePress" src="https://liuyuyang.net/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fanimals.65eaf6e3.webp&w=750&q=75" style="width: 750px; height: 80px"><span/>`,
+    // customHtml: `<span id="runtime"></span>`, // 需要搭配 .vitepress/theme/helper/useRuntime.ts 使用
+    // topMessage: [
+    //   `<span><img alt="VitePress" src="https://liuyuyang.net/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fanimals.65eaf6e3.webp&w=750&q=75" style="width: 750px; height: 80px"><span/>`,
 
-      `<a title="Github release" target="_blank" href="https://github.com/Kele-Bingtang/vitepress-theme-teek/releases" style="margin-right: 10px;">
-        <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/Kele-Bingtang/vitepress-theme-teek?logo=github">
-      </a>
+    //   `<a title="Github release" target="_blank" href="https://github.com/Kele-Bingtang/vitepress-theme-teek/releases" style="margin-right: 10px;">
+    //     <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/Kele-Bingtang/vitepress-theme-teek?logo=github">
+    //   </a>
 
-      <a title="Npm Version" target="_blank" href="https://www.npmjs.com/package/vitepress-theme-teek" style="margin-right: 10px;">
-        <img src="https://img.shields.io/npm/v/vitepress-theme-teek?logo=npm&color=%09%23bf00ff" alt="https://img.shields.io/npm/v/vitepress-theme-teek?color=%09%23bf00ff">
-      </a>
+    //   <a title="Npm Version" target="_blank" href="https://www.npmjs.com/package/vitepress-theme-teek" style="margin-right: 10px;">
+    //     <img src="https://img.shields.io/npm/v/vitepress-theme-teek?logo=npm&color=%09%23bf00ff" alt="https://img.shields.io/npm/v/vitepress-theme-teek?color=%09%23bf00ff">
+    //   </a>
 
-      <img src="https://img.shields.io/badge/v18.x-x?logo=node.js&label=node" alt="node version" style="margin-right: 10px; margin-bottom: 10px;">
-      <img src="https://img.shields.io/github/languages/code-size/Kele-Bingtang/vitepress-theme-teek?logo=Visual Studio Code&logoColor=blue" alt="GitHub code size in bytes" style="margin-right: 10px; margin-bottom: 10px;">
+    //   <img src="https://img.shields.io/badge/v18.x-x?logo=node.js&label=node" alt="node version" style="margin-right: 10px; margin-bottom: 10px;">
+    //   <img src="https://img.shields.io/github/languages/code-size/Kele-Bingtang/vitepress-theme-teek?logo=Visual Studio Code&logoColor=blue" alt="GitHub code size in bytes" style="margin-right: 10px; margin-bottom: 10px;">
 
-      <a title="GitHub Discussions" target="_blank" href="https://github.com/Kele-Bingtang/vitepress-theme-teek/discussions" style="margin-right: 10px;">
-        <img src="https://img.shields.io/github/discussions/Kele-Bingtang/vitepress-theme-teek?color=9cf&logo=github" alt="GitHub Discussions">
-      </a>
+    //   <a title="GitHub Discussions" target="_blank" href="https://github.com/Kele-Bingtang/vitepress-theme-teek/discussions" style="margin-right: 10px;">
+    //     <img src="https://img.shields.io/github/discussions/Kele-Bingtang/vitepress-theme-teek?color=9cf&logo=github" alt="GitHub Discussions">
+    //   </a>
 
-      <a title="MIT License" target="_blank" href="https://github.com/Kele-Bingtang/vitepress-theme-teek/blob/master/LICENSE" style="margin-right: 10px;">
-        <img src="https://img.shields.io/badge/license-MIT-green.svg" alt="MIT License">
-      </a>`,
-    ],
+    //   <a title="MIT License" target="_blank" href="https://github.com/Kele-Bingtang/vitepress-theme-teek/blob/master/LICENSE" style="margin-right: 10px;">
+    //     <img src="https://img.shields.io/badge/license-MIT-green.svg" alt="MIT License">
+    //   </a>`,
+    // ],
   },
-  footerGroup: [
-    {
-      title: "外部链接",
-      links: [
-        { name: "示例 1", link: "https://vp.teek.top" },
-        { name: "示例 2", link: "https://vp.teek.top" },
-        { name: "示例 3", link: "https://vp.teek.top" },
-      ],
-    },
-    {
-      title: "内部链接",
-      links: [
-        { name: "快速开始", link: "/guide/quickstart" },
-        { name: "配置简介", link: "/reference/config" },
-      ],
-    },
-  ],
-  friendLink: {
-    list: [
-      {
-        name: "Teeker",
-        desc: "朝圣的使徒，正在走向编程的至高殿堂！",
-        avatar: "https://testingcf.jsdelivr.net/gh/Kele-Bingtang/static/user/avatar2.png",
-        link: "http://notes.teek.top/",
-      },
-      {
-        name: "vuepress-theme-vdoing",
-        desc: "🚀一款简洁高效的VuePress 知识管理&博客 主题",
-        avatar: "https://doc.xugaoyi.com/img/logo.png",
-        link: "https://doc.xugaoyi.com/",
-      },
-      {
-        name: "One",
-        desc: "明心静性，爱自己",
-        avatar: "https://onedayxyy.cn/img/xyy.webp",
-        link: "https://onedayxyy.cn/",
-      },
-      {
-        name: "Hyde Blog",
-        desc: "人心中的成见是一座大山",
-        avatar: "https://teek.seasir.top/avatar/avatar.webp",
-        link: "https://teek.seasir.top/",
-      },
-      {
-        name: "二丫讲梵",
-        desc: "💻学习📝记录🔗分享",
-        avatar: "https://wiki.eryajf.net/img/logo.png",
-        link: " https://wiki.eryajf.net/",
-      },
-      {
-        name: "粥里有勺糖",
-        desc: "简约风的 VitePress 博客主题",
-        avatar: "https://theme.sugarat.top/logo.png",
-        link: "https://theme.sugarat.top/",
-      },
-      {
-        name: "VitePress 快速上手中文教程",
-        desc: "如果你也想搭建它，那跟我一起做吧",
-        avatar: "https://avatars.githubusercontent.com/u/90893790?v=4",
-        link: "https://vitepress.yiov.top/",
-      },
-      {
-        name: "友人A",
-        desc: "おとといは兎をみたの，昨日は鹿，今日はあなた",
-        avatar: "http://niubin.site/logo.jpg",
-        link: "http://niubin.site/",
-      },
-    ],
-    autoScroll: true,
-  },
+  // footerGroup: [
+  //   {
+  //     title: "外部链接",
+  //     links: [
+  //       { name: "示例 1", link: "https://vp.teek.top" },
+  //       { name: "示例 2", link: "https://vp.teek.top" },
+  //       { name: "示例 3", link: "https://vp.teek.top" },
+  //     ],
+  //   },
+  //   {
+  //     title: "内部链接",
+  //     links: [
+  //       { name: "快速开始", link: "/guide/quickstart" },
+  //       { name: "配置简介", link: "/reference/config" },
+  //     ],
+  //   },
+  // ],
+  // friendLink: {
+  //   list: [
+  //     {
+  //       name: "Teeker",
+  //       desc: "朝圣的使徒，正在走向编程的至高殿堂！",
+  //       avatar: "https://testingcf.jsdelivr.net/gh/Kele-Bingtang/static/user/avatar2.png",
+  //       link: "http://notes.teek.top/",
+  //     },
+  //     {
+  //       name: "vuepress-theme-vdoing",
+  //       desc: "🚀一款简洁高效的VuePress 知识管理&博客 主题",
+  //       avatar: "https://doc.xugaoyi.com/img/logo.png",
+  //       link: "https://doc.xugaoyi.com/",
+  //     },
+  //     {
+  //       name: "One",
+  //       desc: "明心静性，爱自己",
+  //       avatar: "https://onedayxyy.cn/img/xyy.webp",
+  //       link: "https://onedayxyy.cn/",
+  //     },
+  //     {
+  //       name: "Hyde Blog",
+  //       desc: "人心中的成见是一座大山",
+  //       avatar: "https://teek.seasir.top/avatar/avatar.webp",
+  //       link: "https://teek.seasir.top/",
+  //     },
+  //     {
+  //       name: "二丫讲梵",
+  //       desc: "💻学习📝记录🔗分享",
+  //       avatar: "https://wiki.eryajf.net/img/logo.png",
+  //       link: " https://wiki.eryajf.net/",
+  //     },
+  //     {
+  //       name: "粥里有勺糖",
+  //       desc: "简约风的 VitePress 博客主题",
+  //       avatar: "https://theme.sugarat.top/logo.png",
+  //       link: "https://theme.sugarat.top/",
+  //     },
+  //     {
+  //       name: "VitePress 快速上手中文教程",
+  //       desc: "如果你也想搭建它，那跟我一起做吧",
+  //       avatar: "https://avatars.githubusercontent.com/u/90893790?v=4",
+  //       link: "https://vitepress.yiov.top/",
+  //     },
+  //     {
+  //       name: "友人A",
+  //       desc: "おとといは兎をみたの，昨日は鹿，今日はあなた",
+  //       avatar: "http://niubin.site/logo.jpg",
+  //       link: "http://niubin.site/",
+  //     },
+  //   ],
+  //   autoScroll: true,
+  // },
   social: [
+    {
+      icon: "icon-qq",
+      name: "QQ",
+      link: "https://qm.qq.com/q/WXI8ntxaow",
+    },
     {
       icon: "icon-github",
       name: "GitHub",
-      link: "https://github.com/kele-bingtang",
-    },
-    {
-      icon: "icon-gitee",
-      name: "Gitee",
-      link: "https://gitee.com/kele-bingtang",
+      link: "https://github.com/fblaze62/fmdocs",
     },
   ],
   themeEnhance: {
